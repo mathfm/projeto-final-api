1cadef7a6fa738c3f8b3ef700852fea7473986f6
Adson Gabriel <99619952+Gato-Capitao@users.noreply.github.com>
2024-02-23 19:15:41 +0000

    Criação da função update

--- src/controllers/User.controller.js
+++ src/controllers/User.controller.js
@@ -1,10 +1,15 @@
 import { database } from "../database/connection.js"
 import { userEntity } from "../entities/User.entities.js";
 
-
-
 export const createUser = async (name, email, password) => {
     await database.sync();
     await userEntity.create({ name, email, password });    
     return userEntity.findAll();
+}
+
+export const updatePassword = async (id, newPassword)=>{
+    await database.sync();
+    const user = await userEntity.findByPk(id);
+    await user.update({password:newPassword});
+    return  "Senha atualizada com sucesso!";
 }

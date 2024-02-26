1edbe43f20d824fe168f9e7d3792b390f2a29a3b
Adson Gabriel <99619952+Gato-Capitao@users.noreply.github.com>
2024-02-26 21:43:10 +0000

    Update Skill.routes.js
    Criação de endpoints

--- src/routes/Skill.routes.js
+++ src/routes/Skill.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
 import { skillCreateValidator } from "../shared/middleware/SkillValidator.js";
-import { createSkill } from "../controllers/Skill.controller.js";
+import { createSkill, getSkill, updateDescrible, updateSkillName, destroySkill } from "../controllers/Skill.controller.js";
 
 export const skillRouter = Router();
 
@@ -9,3 +9,18 @@ skillRouter.post("/register-skill", skillCreateValidator, async (req, res) => {
     return await createSkill(req, res);
 });
 
+skillRouter.post("/skill/get", async (req, res) => {
+    return await getSkill(req, res);
+})
+
+skillRouter.post("/skill/update-name", async (req, res) => {
+    return await updateSkillName(req, res);
+})
+
+skillRouter.post("/skill/update-describle", async (req, res) => {
+    return await updateDescrible(req, res);
+})
+
+skillRouter.post("/skill/delete-me", async (req, res) => {
+    return await destroySkill(req, res);
+})

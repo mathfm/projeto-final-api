1325c400d1860f7844071b8b623c2a3b213d3a44
Adson Gabriel <99619952+Gato-Capitao@users.noreply.github.com>
2024-02-26 22:19:31 +0000

    Update Skill.controller.js
    Removendo sistema de desmenbramento e verificações de erro

--- src/controllers/Skill.controller.js
+++ src/controllers/Skill.controller.js
@@ -5,57 +5,37 @@ import { skillEntities } from "../entities/Skill.entities.js";
 export const createSkill = async (req, res) => {
     try {
         await database.sync();
-        const { skill_name, describle_skill } = req.body;
-        await skillEntities.create({ skill_name, describle_skill });
+        const { skill_name, describle } = req.body;
+        await skillEntities.create({ skill_name, describle });
         return res.status(201).json({ message: "skill registrada com sucesso" });
     } catch (error) {
         return res.status(404).json({ error: error.message });
     }
 };
 
-export const getSkill = async (req, res) => {
-    try{
-        await database.sync();
-        const {id} = req.body;
-        const skill = await skillEntities.findByPk(id);
-        return skill;
-    }catch(erro){
-        return res.json({erro: erro.message})
-    }
+export const getSkill = async (id) => {
+    await database.sync();
+    const skill = await skillEntities.findByPk(id);
+    return skill;
 }
 
-export const updateDescrible = async (req, res) =>{
-    try{
-        await database.sync();
-        const {id, newDescrible} = req.body;
-        const skill = await skillEntities.findByPk(id);
-        await skill.update({describle:newDescrible});
-        return  "Descrição atualizada com sucesso";
-    }catch(erro){
-        return res.json({erro: erro.message})
-    }
+export const updateDescrible = async (id, newDescrible) =>{
+    await database.sync();
+    const skill = await skillEntities.findByPk(id);
+    await skill.update({describle:newDescrible});
+    return  "Descrição atualizada com sucesso";
 };
 
-export const updateSkillName = async (req, res) =>{
-    try{
-        await database.sync();
-        const { id, newName } = req.body;
-        const skill = await skillEntities.findByPk(id);
-        await skill.update({skill_name:newName});
-        return  "Nome atualizado com sucesso!";
-    }catch(erro){
-        return res.json({erro: erro.message})
-    }
+export const updateSkillName = async (id, newName) =>{
+    await database.sync();
+    const skill = await skillEntities.findByPk(id);
+    await skill.update({skill_name:newName});
+    return  "Nome atualizado com sucesso!";
 };
 
-export const destroySkill = async (req, res)=>{
-    try{
-        await database.sync();
-        const { id } = req.body
-        const skill = await skillEntities.findByPk(id);
-        await skill.destroy();
-        return  "Habilidade deletada!";
-    }catch(erro){
-        return res.json({erro: erro.message})
-    }
+export const destroySkill = async (id)=>{
+    await database.sync();
+    const skill = await skillEntities.findByPk(id);
+    await skill.destroy();
+    return  "Habilidade deletada!";
 }

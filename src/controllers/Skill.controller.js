38363d4909d23e399aa68c910bca066e7c4f8c79
Adson Gabriel <99619952+Gato-Capitao@users.noreply.github.com>
2024-02-27 21:44:06 +0000

    Update Skill.controller.js
    Ajustando status dos retornos.

--- src/controllers/Skill.controller.js
+++ src/controllers/Skill.controller.js
@@ -18,7 +18,7 @@ export const getSkill = async (req, res) => {
         await database.sync();
         const {id} = req.body;
         const skill = await skillEntities.findByPk(id);
-        return skill;
+        return res.status(200).json({skill});
     } catch(error){
         return res.status(404).json({ error: error.message });
     }
@@ -30,7 +30,7 @@ export const updateDescrible = async (req, res) =>{
         const {id, newDescrible} = req.body;
         const skill = await skillEntities.findByPk(id);
         await skill.update({describle:newDescrible});
-        return res.status(201).json({ message: "Descrição atualizada com sucesso" });
+        return res.status(200).json({ message: "Descrição atualizada com sucesso" });
     } catch(error){
         return res.status(404).json({ error: error.message });
     }
@@ -43,19 +43,19 @@ export const updateSkillName = async (req, res) =>{
         const { id, newName } = req.body;
         const skill = await skillEntities.findByPk(id);
         await skill.update({skill_name:newName});
-        return res.status(201).json({ message: "Nome atualizado com sucesso!"});
+        return res.status(200).json({ message: "Nome atualizado com sucesso!"});
     } catch(error){
         return res.status(404).json({ error: error.message });
     }
 };
 
-export const destroySkill = async (id)=>{
+export const destroySkill = async (req, res)=>{
     try{
         await database.sync();
         const { id } = req.body;
         const skill = await skillEntities.findByPk(id);
         await skill.destroy();
-        return res.status(201).json({ message: "Habilidade deletada!"});
+        return res.status(200).json({ message: "Habilidade deletada!"});
     } catch(error){
         return res.status(404).json({ error: error.message });
     }
